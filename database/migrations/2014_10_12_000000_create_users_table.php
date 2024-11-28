4af7eff2c39764a808e4215f53d7b6d2204d618f
Ismi <ismi.putri397@gmail.com>
2024-11-28 23:35:09 +0800

    travel_packages migration done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique(); //batasi panjang ke 191 char
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
